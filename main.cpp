0d870746455db03d325ed9185a2a96fb7ef92ad5
glory727 <1169701980@qq.com>
2018-08-17 11:12:28 +0800

    Signed-off-by: glory727 <1169701980@qq.com>

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world!" << endl;
+	const char *filename = "coco.names";
+	FILE *file = fopen(filename, "r");
+	if (!file)
+		return 0;
+	
+	size_t size = 512;
+	char *line = (char *)malloc(size * sizeof(char));
+	if (!fgets(line, size, file)) {
+		int a = 1;
+		return 0;
+	}
+	else
+		cout << line << endl;
+
+	system("pause");
+	return 0;
+}
